e6f186e64a05c28cc5ce1f0e6000cf9b357d73f0
German <you@example.com>
2015-12-20 23:50:39 +0100

    Usuario Implementado en banco_api

---
+++ src/java/com/fpmislata/presentacion/controladores/UsuarioController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.presentacion.controladores;
+
+
+import com.fpmislata.banco.business.service.UsuarioService;
+import com.fpmislata.banco.core.BusinessException;
+import com.fpmislata.banco.core.BusinessMessage;
+import com.fpmislata.presentacion.json.JsonTransformer;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Equipo
+ */
+@Controller
+public class UsuarioController {
+    
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    JsonTransformer jsonTransformer;
+    
+    @RequestMapping(value = "/usuario", method = RequestMethod.GET, produces = "application/json")
+    public void findall(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        try {
+
+            String jsonUsuario;
+            if (httpServletRequest.getParameter("nombre") != null) {
+                jsonUsuario = jsonTransformer.toJson(usuarioService.findByNombre(httpServletRequest.getParameter("nombre")));
+            } else {
+                jsonUsuario = jsonTransformer.toJson(usuarioService.findAll());
+            }
+
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            httpServletResponse.getWriter().println(jsonUsuario);
+
+        } catch (BusinessException ex) {
+            List<BusinessMessage> businessMessages = ex.getBusinessMessages();
+            String jsonSalida = jsonTransformer.toJson(businessMessages);
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            try {
+                httpServletResponse.getWriter().println(jsonSalida);
+            } catch (IOException ex1) {
+                Logger.getLogger(EntidadBancariaController.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+
+        } catch (Exception ex) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            try {
+                ex.printStackTrace(httpServletResponse.getWriter());
+            } catch (IOException ex1) {
+                Logger.getLogger(EntidadBancariaController.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
+
+    }
+    
+    
+}
