ae1d1ec7bf4fbdec9a4ee4dcd2708b9882c615fb
Your Name <you@example.com>
2016-01-12 10:39:15 +0100

    Mejora Login

--- src/java/com/fpmislata/presentacion/controladores/LoginController.java
+++ src/java/com/fpmislata/presentacion/controladores/LoginController.java
@@ -8,11 +8,16 @@ package com.fpmislata.presentacion.controladores;
 
 import com.fpmislata.banco.business.domain.Usuario;
 import com.fpmislata.banco.business.service.UsuarioService;
+import com.fpmislata.banco.core.BusinessException;
+import com.fpmislata.banco.core.BusinessMessage;
 import com.fpmislata.banco.security.PasswordManager;
 import com.fpmislata.presentacion.json.JsonTransformer;
 import com.fpmislata.presentacion.security.WebSession;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -41,8 +46,8 @@ public class LoginController {
     public void login(@RequestBody String jsonEntrada, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
         try {
             Usuario usuario = jsonTransformer.fromJSON(jsonEntrada, Usuario.class);
-            if (usuario == null){
-                 httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            if (usuario.getNickName().isEmpty() || usuario.getPasswordEncrypt().isEmpty()){
+                   throw new BusinessException("Los campos NickName o Password no pueden estar vacios","Usuario");
             }else{
             if(passwordManager.check(usuario.getPasswordEncrypt(), usuarioService.findByNickName(usuario.getNickName()).getPasswordEncrypt() )){
             WebSession webSession = new WebSession(usuario, new Date());
@@ -52,11 +57,21 @@ public class LoginController {
             httpServletResponse.getWriter().println(jsonTransformer.toJson(usuarioService.findByNickName(usuario.getNickName())));
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
             }else{
-                httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                throw new BusinessException("El password introducido es incorrecto","Usuario");
             }
             }
-        } catch (Exception ex) {
+            
+        }catch (BusinessException ex) {
+            List<BusinessMessage> businessMessages = ex.getBusinessMessages();
+            String jsonSalida = jsonTransformer.toJson(businessMessages);
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            try {
+                httpServletResponse.getWriter().println(jsonSalida);
+            } catch (IOException ex1) {
+                Logger.getLogger(EntidadBancariaController.class.getName()).log(Level.SEVERE, null, ex1);
+            } catch (Exception ex1) {
             ex.printStackTrace(httpServletResponse.getWriter());
         }
-    }
-}
+    }   
+}}
